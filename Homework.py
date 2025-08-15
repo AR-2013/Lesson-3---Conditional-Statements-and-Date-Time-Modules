a48660ac22ebfb7d2c27c9b742f06e8141003808
AR-2013 <anairarandeva@icloud.com>
2025-08-15 11:02:13 +0400

    Homework

---
+++ Homework.py
@@ -0,0 +1,9 @@
+x = int(input("What is the number of days" \
+" that you have gone to school:"))
+
+if x>=75:
+ print("You are eligible to do the test.")
+
+else:
+ print("Youe are not eligible to do the test.")
+ 
